23dd9afbb8e70b0c5c67da7eb7e7088595b3735e
Rafał Barnaś <125920366+rapheeck@users.noreply.github.com>
2023-02-26 17:22:48 +0100

    corected functions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 {
-    const currencyValue = document.querySelector(".js-value");
-    const exchangeRate = document.querySelector(".js-rate");
-    const formElement = document.querySelector(".js-form");
-    const currencyAmount = document.querySelector(".js-amount");
-    const resetAmount = document.querySelector('.js-reset');
-
-
-    formElement.addEventListener("input", () => {
-        const amount = +currencyValue.value;
-        const rate = +exchangeRate.value;
-        const zlotyAmount = amount * rate;
-        currencyAmount.innerText = zlotyAmount.toFixed(2);
-    });
-
-    resetAmount.addEventListener("click", () => {
+    const resetAmount = () => {
+        resetAmount = document.querySelector(".js-reset");
         currencyAmount.innerText = 0.00;
-    });
-    resetAmount = document.querySelector(".js-reset");
+    };
+
+    const onChange = () => {
+        const currencyValue = document.querySelector(".js-value");
+        const exchangeRate = document.querySelector(".js-rate");
+        const currencyAmount = document.querySelector(".js-amount");
+        const zlotyAmount = +currencyValue.value * +exchangeRate.value;
+        return currencyAmount === zlotyAmount.toFixed(2);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+        const resetAmount = document.querySelector('.js-reset');
+        formElement.addEventListener("input", onChange);
+        resetAmount.addEventListener("click", resetAmount);
+    };
+
+    init();
 }
+
+// formElement.addEventListener("input", onChange);
+
+// resetAmount.addEventListener("click", resetAmount);
